cc1eb5037349b7848ec97ad63dbf735ed885d362
WasimHamid <44105571+WasimHamid@users.noreply.github.com>
2020-06-24 21:49:01 +0100

    Update index.js

--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -24,7 +24,6 @@ export default class Details extends Component {
                                 </div>
 
                                 <div className='col-10 mx-auto col-md-6 my-3 text-capitalize'>
-                                    <h2>model : {title}</h2>
                                     <h4 className='text-blue'>
                                         <strong>
                                             price : <span>£</span>{price}
